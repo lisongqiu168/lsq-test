f47be7c988c3436a0056448138c6ac00172672c6
lisongqiu <lisongqiu@idvert.com>
2018-01-22 17:23:50 +0800

    kafka

--- src/main/java/com/lsq/controller/UserController.java
+++ src/main/java/com/lsq/controller/UserController.java
@@ -1,26 +1,15 @@
 package com.lsq.controller;
 
-import java.util.Iterator;
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.http.HttpStatus;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.mongodb.DBCollection;
-import com.mongodb.DBObject;
-import com.mongodb.util.JSON;
 
 @RestController
 @RequestMapping("/user")
@@ -38,9 +27,10 @@ public class UserController {
 			UserParam user) {
 		// kafka
 		ListenableFuture<SendResult<String, String>> r = kafkaTemplate
-				.sendDefault("test_value");
-		// String s = com.alibaba.fastjson.JSON.toJSONString(user);
-		// mongoTemplate.insert(JSON.parse(s));
+				.send("test_topic","test_value");
+//		String s = com.alibaba.fastjson.JSON.toJSONString(user);
+//		mongoTemplate.insert(
+//				com.alibaba.fastjson.JSON.parseObject(s, UserParam.class));
 		// List<UserParam> list = mongoTemplate.findAll(UserParam.class);
 		// for (UserParam userParam : list) {
 		// System.out.println(userParam.getUserId());
